4010795441fac1db591f0469be2b7e77cad3cc39
Jamal Id Aissa <idaissa93@gmail.com>
2025-05-05 20:14:22 +0100

    feat: update meta tags for improved SEO and social sharing

--- web/app/routes/home.tsx
+++ web/app/routes/home.tsx
@@ -1,10 +1,19 @@
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
+import splashDarkImg from "images/swiss-knife-learning-3d.png";
 
 export function meta({}: Route.MetaArgs) {
   return [
     { title: "ELearnKit - AI-driven platform" },
-    { name: "description", content: "Welcome to React Router!" },
+    { name: "description", content: " Smart, personalized learning powered by semantic technology." },
+    {
+      property: "og:image",
+      content: splashDarkImg,
+    },
+    {
+      property: "og:title",
+      content: "ELearnKit - AI-driven platform",
+    },
   ];
 }
 
